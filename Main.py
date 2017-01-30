4c8bff62000d2a9fe68bbef435783ba0826df42d
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 19:32:47 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,17 +12,27 @@ def Draw():
             if X[0] == 1: DISP.blit(Tile, (x,y))
             elif X[0] == 2: DISP.blit(Wall, (x,y))
                 
-def Draw_Cords():
-    print()
+def DrawSquare(X1,Y1,X2,Y2):
+    x = X1*24; y = Y1*24
+    for Line in range(abs(X2-X1+1)):
+        x = X1*24
+        y += TileSize
+        for X in range(abs(Y2-Y1+1)):
+            x += TileSize
+            DISP.blit(Tile, (x,y))
     
-def Draw_Humie():
+def DrawHumie():
     DISP.blit(Humie,(HumieX*24,HumieY*24))
 
 pygame.init()
 pygame.display.set_caption('I LIKE TO MOVE IT MOVE IT')
 
 MAP = Gen.Create_Map(16,12)
-Draw(); Draw_Humie()
+#Draw(); 
+DrawSquare(abs(Room[0]),abs(Room[1]),abs(Room[2]),abs(Room[3]))
+DrawHumie()
+
+Sq = Func.MakeSquaresInCircle(25,10)
 
 while True:
     
@@ -43,11 +53,15 @@ while True:
             if event.key == pygame.K_DOWN:
                 HumieY += 1
                 Humie = HumieDown
-                       
+                
             DISP.fill(BLACK) 
-            Draw(); Draw_Humie()
+            for Room in Sq:
+                DrawSquare(abs(Room[0]),abs(Room[1]),abs(Room[2]),abs(Room[3]))       
             
-            t = Gen.RandomPointInCircle(10)
+            #Draw(); 
+            DrawHumie()
+            
+            
             
     pygame.display.update()
     FpsClock.tick(FPS)        
