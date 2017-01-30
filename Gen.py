b1c60807e750d824dcf56766a1213e52e1c42022
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 16:29:37 +0300

    Create Gen.py

---
+++ Gen.py
@@ -0,0 +1,13 @@
+# Returns a blank map
+def Create_Map(x,y):
+    Map = []
+    for i in range(y):
+        Line = []
+        for j in range(x):
+            # Type, Passable, Transparrant, Scouted 
+            n = [1,False,False,False]
+            Line.append(n)
+        Map.append(Line)
+    return(Map)
+            
+        
