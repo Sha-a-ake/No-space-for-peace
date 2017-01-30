c4abbb712340e53e16c6ae62adbb0c9dea769fb0
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 21:12:51 +0300

    Update Func.py

--- Func.py
+++ Func.py
@@ -34,48 +34,5 @@ def MakeSquaresInCircle(Rooms,Radius):
         Sq.append(MakeASquare(Radius))
     return(Sq)
     
-def TupToLst(tup):
-    lis = []
-    for tuplette in tup:
-        lis.append(list(tuplette))
-    return(lis)
-        
-    
-def Spread(Rooms):
-    for z in range(len(Rooms)-1):
-        for i in range(z,len(Rooms)):
-            while CollisionTrue(Rooms[z][0],Rooms[z][1],Rooms[z][2],Rooms[z][3],
-                             Rooms[i][0],Rooms[i][1],Rooms[i][2],Rooms[i][3],):
-                x1,y1 = SquareCenter(Rooms[z][0],Rooms[z][1],Rooms[z][2],Rooms[z][3])
-                x2,y2 = SquareCenter(Rooms[i][0],Rooms[i][1],Rooms[i][2],Rooms[i][3])
-                print(x1,y1,x2,y2)
-                if abs(x1 - x2) >= abs(y1-y2):
-                    if x1 > x2:
-                        Rooms[z][0] += 1
-                        #Rooms[z][1] -= 1
-                        Rooms[z][2] += 1
-                        #Rooms[z][3] -= 1
-                        print('Mov left')
-                    elif x1 < x2:
-                        Rooms[z][0] -= 1
-                        #Rooms[z][1] += 1
-                        Rooms[z][2] -= 1
-                        #Rooms[z][3] += 1
-                        print('Mov right')
-        
-                else:
-                    if y1 > y2:
-                        #Rooms[z][0] -= 1
-                        Rooms[z][1] += 1
-                        #Rooms[z][2] -= 1
-                        Rooms[z][3] += 1
-                        print('Mov up')
-                    elif y2 > y1:
-                        #Rooms[z][0] += 1
-                        Rooms[z][1] -= 1
-                        #Rooms[z][2] += 1
-                        Rooms[z][3] -= 1
-                        print('Mov down')
-                print('Loop!', z,i)
-    return Rooms
+
     
