90025395321d2063284ec0aeeb03e39a5e8d62cd
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 22:13:00 +0300

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -4,8 +4,8 @@ def Create_Map(x,y):
     for i in range(y):
         Line = []
         for j in range(x):
-            # Type, Passable, Transparrant, Scouted 
-            n = [1,False,False,False]
+            # Type,Occupied, Passable, Transparrant, Scouted 
+            n = [1,0,True,False,False]
             Line.append(n)
         Map.append(Line)
     return(Map)
