9ce7ab3229f5692227f88dd8bf8b8ee43afeda5c
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 16:29:18 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
-import pygame, sys
+import pygame, sys, Gen
 from pygame.locals import *
 
 FPS = 30
 FpsClock = pygame.time.Clock()
+TileSize = 24
 
 pygame.init()
 DISP = pygame.display.set_mode((640,480),0,32)
@@ -14,29 +15,18 @@ WHITE = (255,255,255)
 RED   = (255,0,0)
 GREEN = (0,255,0)
 BLUE  = (0,0,255)
-
-MAP = [
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-]
-
-px = 4; py = 4;
-
 Tile = pygame.image.load('floor.png')
+
+MAP = Gen.Create_Map(20,2)
 
 def draw(o,s):
     x = o; y = s
     for Line in MAP:
         x = o
-        y += 16
+        y += TileSize
         for X in Line:
-            x += 16
-            if X == 0: DISP.blit(Tile, (x,y))
+            x += TileSize
+            if X[0] == 1: DISP.blit(Tile, (x,y))
 
 draw(0,0)
 xx = 0; yy = 0
