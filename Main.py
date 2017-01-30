d819bb2e230b0d16f69cda43ca433621481d561e
Sha-a-ake <Shake3720@gmail.com>
2017-01-30 14:52:28 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,33 @@
-import pygame, sys, Init
+import pygame, sys
 from pygame.locals import *
+
 pygame.init()
 DSP = pygame.display.set_mode((600,400))
 pygame.display.set_caption('Hello World!')
 
-# Setting up the colors
-Black = (0,0,0)
-White = (255,255,255)
-Red   = (255,0,0)
-Green = (0,255,0)
-Blue  = (0,0,255)
 
-DSP.fill(White)
-pygame.draw.polygon(DSP, Red, ((146,0),(291,106),(236,277),(56,277),(0,106)))
-pygame.draw.line(DSP,Blue,(60,60),(120,60),4)
+# Setting up the colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED   = (255,0,0)
+GREEN = (0,255,0)
+BLUE  = (0,0,255)
+
+pygame.draw.polygon(DSP, GREEN, ((146, 0), (291, 106),(236,277),(56, 277),(0,106)))
+pygame.draw.line(DSP, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(DSP, BLUE, (120, 60), (60, 120))
+pygame.draw.line(DSP, BLUE, (60, 120), (120, 120), 4)
+pygame.draw.circle(DSP, BLUE, (300, 50), 20, 0)
+pygame.draw.ellipse(DSP, RED, (300, 250, 40, 80), 1)
+pygame.draw.rect(DSP, RED, (200, 150, 100, 50))
+
+pix = pygame.PixelArray(DSP)
+pix[480][380] = BLACK
+pix[482][382] = BLACK
+pix[484][384] = BLACK
+pix[486][386] = BLACK
+pix[488][388] = BLACK
+del pix
 
 while True:
     for event in pygame.event.get():
