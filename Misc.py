e7f595893d0a4df2ccf77371338d47f23ecfc687
Sha-a-ake <Shake3720@gmail.com>
2017-02-06 21:45:31 +0200

    Add files via upload

---
+++ Misc.py
@@ -0,0 +1,29 @@
+# --- Does not work yet --- #
+
+import pygame
+from Const import *
+import Config as C
+
+class bullet:
+    Model = BulletHor
+    def __init__(self,x,y,direction,power):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.power = power
+        
+    def Move(self):
+        if direction == 'up':
+            self.y -= 1
+        elif direction == 'down':
+            self.y += 1
+        elif direction == 'right':
+            self.x += 1
+        else:
+            self.x -= 1
+            
+    def Draw(self):
+        DISPLAY.blit(self.Model,((self.x+1)*24,(self.y+1)*24))
+        
+        
+        
