8e28e35380b84e504e53918ed15441f200dd8a91
Sha-a-ake <Shake3720@gmail.com>
2017-02-07 22:59:28 +0300

    Update Misc.py

--- Misc.py
+++ Misc.py
@@ -6,22 +6,31 @@ import Config as C
 
 class bullet:
     Model = BulletHor
+    Alive = True
     
     def __init__(self,x,y,direction,power):
         self.x = x
         self.y = y
         self.direction = direction
         self.power = power
+    
+    def Impact(self):
+        if   C.Map[self.x][self.y][2] == False: self.Alive = False
+        #elif C.Map[self.x][self.y][3] == False:
         
+        
+            
     def Move(self):
-        if self.direction == 'up':
-            self.y -= 1
+        if   self.direction == 'up':
+             self.y -= 1
         elif self.direction == 'down':
-            self.y += 1
+             self.y += 1
         elif self.direction == 'right':
-            self.x += 1
+             self.x += 1
         else:
-            self.x -= 1
+             self.x -= 1
+        print(self.x,self.y)
+        self.Impact()
             
     def Draw(self):
         DISPLAY.blit(self.Model,((self.x+1)*24,(self.y+1)*24))
