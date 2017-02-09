3bc06eb9bdc8c7a09b639f1aee4ba3c49e6f7bd7
Sha-a-ake <Shake3720@gmail.com>
2017-02-09 11:27:03 +0200

    Add files via upload

---
+++ Player.py
@@ -0,0 +1,38 @@
+import Config as C
+from Images import *
+
+def Move(direction):
+    C.Move = True
+    if direction == 'right':
+        C.Humie = HumieRight
+        if C.Map[C.HumieX+1][C.HumieY][2] == True and C.Map[C.HumieX+1][C.HumieY][3] == True:
+            C.HumieX += 1        
+    elif direction == 'left':
+        C.Humie = HumieLeft
+        if C.Map[C.HumieX-1][C.HumieY][2] == True and C.Map[C.HumieX-1][C.HumieY][3] == True:
+            C.HumieX -= 1        
+    elif direction == 'up':
+        C.Humie = HumieUp
+        if C.Map[C.HumieX][C.HumieY-1][2] == True and C.Map[C.HumieX][C.HumieY-1][3] == True:
+            C.HumieY -= 1
+    elif direction == 'down':
+        C.Humie = HumieDown
+        if C.Map[C.HumieX][C.HumieY+1][2] == True and C.Map[C.HumieX][C.HumieY+1][3] == True:
+            C.HumieY += 1
+            
+    def Attack():
+        C.Move = True
+        if C.Humie == HumieLeft:
+            for Bot in C.Bots:
+                Bot.GetHit(C.HumieX-1,C.HumieY,30)
+        if C.Humie == HumieRight:
+            for Bot in C.Bots:
+                Bot.GetHit(C.HumieX+1,C.HumieY,30)
+        if C.Humie == HumieUp:
+            for Bot in C.Bots:
+                Bot.GetHit(C.HumieX,C.HumieY-1,30)
+        if C.Humie == HumieDown:
+            for Bot in C.Bots:
+                Bot.GetHit(C.HumieX,C.HumieY+1,30)    
+    def hi():
+        print('hi')   
